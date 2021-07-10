41ac33b4a1df5e317fa81852f2873d39967b71fb
Najla Alshehri <85630240+Najlav@users.noreply.github.com>
2021-07-10 05:39:12 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'najlas-coffee-shop.us.auth0.com', // the auth0 domain prefix
+    audience: 'coffeeShop_API', // the audience set for the auth0 app
+    clientId: 'y8JECaYjeT4KD7iJX5DRrCXbJS7vC9X8', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
